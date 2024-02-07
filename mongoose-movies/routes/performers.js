d94f090aa698e4c03e47ebde62fc30c8acbdf598
zynbahmed <201901241@student.polytechnic.bh>
2024-02-07 12:34:31 +0300

    add authorization

--- mongoose-movies/routes/performers.js
+++ mongoose-movies/routes/performers.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const performersCtrl = require('../controllers/performers');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // This router is mounted to a "starts with" path of '/'
 
 // GET /performers/new (new functionality)
-router.get('/performers/new', performersCtrl.new);
+router.get('/performers/new', ensureLoggedIn, performersCtrl.new);
 // POST /performers (create functionality)
-router.post('/performers', performersCtrl.create);
+router.post('/performers', ensureLoggedIn, performersCtrl.create);
 // POST /movies/:id/performers (associate performers with movies)
 router.post('/movies/:id/performers', performersCtrl.addToCast)
 
--- mongoose-movies/routes/reviews.js
+++ mongoose-movies/routes/reviews.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reviewsCtrl = require('../controllers/reviews');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
+
 
 // POST /movies/:id/reviews (create review for a movie)
-router.post('/movies/:id/reviews', reviewsCtrl.create);
+router.post('/movies/:id/reviews', ensureLoggedIn, reviewsCtrl.create);
 
-router.delete('/reviews/:id', reviewsCtrl.delete);
+router.delete('/reviews/:id', ensureLoggedIn, reviewsCtrl.delete);
 
 module.exports = router;
