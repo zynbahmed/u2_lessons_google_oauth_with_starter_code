42a0adc2cb00fd90e897a917b670666f5a446fc5
zynbahmed <201901241@student.polytechnic.bh>
2024-02-07 12:21:58 +0300

    updating the review ui

--- mongoose-movies/controllers/reviews.js
+++ mongoose-movies/controllers/reviews.js
@@ -6,6 +6,11 @@ module.exports = {
 
 async function create(req, res) {
   const movie = await Movie.findById(req.params.id);
+
+  req.body.user = req.user._id;
+  req.body.userName = req.user.name;
+  req.body.userAvatar = req.user.avatar;
+  
   // We can push (or unshift) subdocs into Mongoose arrays
   movie.reviews.push(req.body);
   try {
